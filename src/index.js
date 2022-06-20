6e7ea28728f666f6e7c925258e1e887512d36073
Yahya <yahyahussein670@gmail.com>
2022-06-20 22:14:02 +0300

    Add a function which fetches the film names and appends to the navbar

--- src/index.js
+++ src/index.js
@@ -5,4 +5,15 @@ function fetchMovies() {
     .then(data => {
         return getfilms(data)
     })
-}   
+}
+fetchMovies()
+
+const getfilms = (films) => {
+    films.forEach(film => {
+        const movietitles = document.querySelector('#films')
+        const movielist = document.createElement("li")
+
+        movielist.textContent = film.title
+        movietitles.appendChild(movielist)
+    });
+}
