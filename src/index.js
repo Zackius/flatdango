f8812ad19207e8b0c5dd87597556964175ab39a8
Yahya <yahyahussein670@gmail.com>
2022-06-20 22:03:32 +0300

    Add a fetch request to get the film data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-// Your code here
+function fetchMovies() {
+    //const navbar = document.querySelector('#beer-list')
+    fetch('http://localhost:3000/films')
+    .then(res => res.json())
+    .then(data => {
+        return getfilms(data)
+    })
+}   
